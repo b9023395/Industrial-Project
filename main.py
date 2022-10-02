fad49335ee89ded13b051873a94f1bfa6040a40a
Sam's PC <s.t.fielding1@ncl.ac.uk>
2022-10-02 14:31:47 +0100

    Separate GUI into separate file and include in main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+import gui
 
-
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        screen = QApplication.primaryScreen()
-        size = screen.size()
-        self.setGeometry(int(size.width()/2)-300, int(size.height()/2)-300, 600, 600)
-        self.setWindowTitle("GUI Window")
-        self.show()
-
-
-app = QApplication(sys.argv)
-window = Window()
-sys.exit(app.exec_())
